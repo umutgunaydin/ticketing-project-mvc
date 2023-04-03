5db7b20b0cfdc46441266d229b5e8c946661a9c7
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-03 03:19:46 +0300

    showing roles on screen is done

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.company.controller;
 
 import com.company.dto.UserDTO;
+import com.company.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService;
+
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
-        //model.addAttribute("roles", RoleSer)
+        model.addAttribute("roles",roleService.findAll());
 
         return "/user/create";
     }
