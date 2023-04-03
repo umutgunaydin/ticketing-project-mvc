bc2fdfafff88ed041e1685cc79d51340bef09735
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-03 03:38:23 +0300

    showing users on screen is done

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -2,6 +2,7 @@ package com.company.controller;
 
 import com.company.dto.UserDTO;
 import com.company.service.RoleService;
+import com.company.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final RoleService roleService;
+    private final UserService userService;
 
-    public UserController(RoleService roleService) {
+    public UserController(RoleService roleService, UserService userService) {
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     @GetMapping("/create")
@@ -22,7 +25,9 @@ public class UserController {
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
 
         return "/user/create";
     }
+
 }
