59d2d17a3d3da84f9f35a1887e25ebf9a2816284
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-05 02:26:42 +0300

    project controller is created

---
+++ src/main/java/com/company/controller/ProjectController.java
@@ -0,0 +1,16 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    @GetMapping("/create")
+    public String createProject(){
+        return "project/create";
+    }
+
+}
