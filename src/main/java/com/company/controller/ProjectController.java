ee4d36c4d4c106331f02e8e874fb13bcebcca09c
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-05 05:04:12 +0300

    project saving is done

--- src/main/java/com/company/controller/ProjectController.java
+++ src/main/java/com/company/controller/ProjectController.java
@@ -1,10 +1,14 @@
 package com.company.controller;
 
 import com.company.dto.ProjectDTO;
+import com.company.enums.Status;
+import com.company.service.ProjectService;
 import com.company.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,18 +16,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProjectController {
 
     private final UserService userService;
+    private final ProjectService projectService;
 
-    public ProjectController(UserService userService) {
+    public ProjectController(UserService userService, ProjectService projectService) {
         this.userService = userService;
+        this.projectService = projectService;
     }
 
     @GetMapping("/create")
     public String createProject(Model model){
 
         model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("managers",userService.findAll().stream());
+        model.addAttribute("managers",userService.findAll());
+        model.addAttribute("projects", projectService.findAll());
 
         return "project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+
+        project.setProjectStatus(Status.OPEN);
+        projectService.save(project);
+
+        return "redirect:/project/create";
     }
 
 }
--- src/main/java/com/company/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/company/service/impl/ProjectServiceImpl.java
@@ -1,15 +1,21 @@
 package com.company.service.impl;
 
 import com.company.dto.ProjectDTO;
+import com.company.enums.Status;
 import com.company.service.CrudService;
 import com.company.service.ProjectService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
     @Override
     public ProjectDTO save(ProjectDTO project) {
-        return super.save(project.getProjectCode(),project);
+        if (project.getProjectStatus() == null) {
+            project.setProjectStatus(Status.OPEN);
+        }
+        return super.save(project.getProjectCode(), project);
     }
 
     @Override
@@ -29,6 +35,6 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> i
 
     @Override
     public void update(ProjectDTO project) {
-        super.update(project.getProjectCode(),project);
+        super.update(project.getProjectCode(), project);
     }
 }
