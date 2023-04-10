83703918c92fa8c56ea9dc15fd11c95556eda56a
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-10 16:21:26 +0300

    validation added

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -5,7 +5,10 @@ import com.company.service.RoleService;
 import com.company.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -30,7 +33,13 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user, Model model){
+    public String insertUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("roles",roleService.findAll());
+            model.addAttribute("users",userService.findAll());
+            return "/user/create";
+        }
 
         userService.save(user);
 
@@ -49,7 +58,7 @@ public class UserController {
     }
 
     @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") UserDTO user){ // without using @ModelAttribute it is ok
+    public String updateUser(@Valid @ModelAttribute("user") UserDTO user){ // without using @ModelAttribute it is ok
 
         userService.update(user);
 
