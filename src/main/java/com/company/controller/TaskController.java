c7ee73f5649535ab33addd95ab424d81115d8534
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-04-10 05:34:29 +0300

    taskCreate created

--- src/main/java/com/company/controller/TaskController.java
+++ src/main/java/com/company/controller/TaskController.java
@@ -1,20 +1,46 @@
 package com.company.controller;
 
+import com.company.dto.TaskDTO;
+import com.company.service.ProjectService;
+import com.company.service.TaskService;
+import com.company.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/task")
 public class TaskController {
 
+    private final TaskService taskService;
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public TaskController(TaskService taskService, UserService userService, ProjectService projectService) {
+        this.taskService = taskService;
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
     @GetMapping("/create")
-    public String createTask(){
+    public String createTask(Model model){
 
-
+        model.addAttribute("task",new TaskDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
         return "task/create";
     }
 
+    @PostMapping("/create")
+    public String insertTask(TaskDTO task){
+
+        taskService.save(task);
+
+        return "redirect:/task/create";
+    }
 
 
 }
--- src/main/java/com/company/service/impl/TaskServiceImpl.java
+++ src/main/java/com/company/service/impl/TaskServiceImpl.java
@@ -1,34 +1,45 @@
 package com.company.service.impl;
 
 import com.company.dto.TaskDTO;
+import com.company.enums.Status;
 import com.company.service.TaskService;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
-
+@Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
 
     @Override
     public TaskDTO save(TaskDTO task) {
-        return null;
+
+        if (task.getTaskStatus() == null){
+            task.setTaskStatus(Status.OPEN);
+        }
+        if (task.getAssignedDate()==null){
+            task.setAssignedDate(LocalDate.now());
+        }
+
+        return super.save(task.getId(),task);
     }
 
     @Override
     public TaskDTO findById(Long id) {
-        return null;
+        return super.findByID(id);
     }
 
     @Override
     public List<TaskDTO> findAll() {
-        return null;
+        return super.findAll();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        super.deleteByID(id);
     }
 
     @Override
     public void update(TaskDTO task) {
-
+        super.update(task.getId(),task);
     }
 }
